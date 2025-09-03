0170298afaf3aa9d236a9c84bec41c28d076297f
-3σ <yurippe@m3sigma.net>
2025-09-04 06:27:24 +0900

    -

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.staticfiles import StaticFiles
 import uvicorn
 
 from api.routes import *
@@ -18,6 +19,7 @@ app.add_middleware(
 app.include_router(auth_router)
 app.include_router(admin_router)
 app.include_router(api_router)
+app.mount('/', StaticFiles(directory='dist', html=True))
 
 
 from sqlalchemy import select
